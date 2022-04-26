36953874b76e94b1d65da7f5fc3a33b6794c6c5e
Aviehu <aviehu@gmail.com>
2022-04-26 15:54:07 +0300

    skeleton

---
+++ EtradeSystem/src/Domain/Stores/PolicyManager.java
@@ -0,0 +1,15 @@
+package Domain.Stores;
+
+import java.util.List;
+
+public class PolicyManager {
+    private List<Discount> ownersDiscounts;
+    private List<Purchase> ownerPurchases;
+    private List<Discount> discounts;
+    private List<Purchase> purchases;
+
+    public PolicyManager() {
+
+    }
+
+}
---
+++ EtradeSystem/src/Domain/Stores/StoresFacade.java
@@ -0,0 +1,14 @@
+package Domain.Stores;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StoresFacade {
+    private List<Store> stores;
+
+    public StoresFacade(){
+        stores = new LinkedList<>();
+    }
+
+
+}
