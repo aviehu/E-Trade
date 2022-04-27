5c34634d0a352858f66396ad496de5fb8edd7c23
BatyaBialik <bialikb@post.bgu.ac.il>
2022-04-27 15:30:30 +0300

    prepare AcceptanceTests

---
+++ EtradeSystem/src/Tests/Acceptance/System/SupplyTest.java
@@ -0,0 +1,17 @@
+package Tests.Acceptance.System;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SupplyTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
