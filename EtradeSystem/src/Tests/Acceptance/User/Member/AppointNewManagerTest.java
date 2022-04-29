f52b670799e3f79273df77ac0a135de45d8ab354
BatyaBialik <bialikb@post.bgu.ac.il>
2022-04-29 13:18:12 +0300

    Acceptance tests - Member- appoint manager\owner

--- EtradeSystem/src/Tests/Acceptance/User/Member/AppointNewManagerTest.java
+++ EtradeSystem/src/Tests/Acceptance/User/Member/AppointNewManagerTest.java
@@ -1,14 +1,48 @@
 package Tests.Acceptance.User.Member;
 
+import Service.SystemService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class AppointNewManagerTest {
+
+    private SystemService systemService;
+
     @Before
     public void setUp() throws Exception {
+        systemService = new SystemService();
+        systemService.enterSystem();
+        String guestName = systemService.getOnline().getVal();
+        systemService.signUp(guestName, "Mira", "200");
+        systemService.signUp(guestName, "Andalus", "100");
+        systemService.login(guestName, "Andalus", "100");
+        systemService.openStore("Andalus", "Mega", 123);
     }
 
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void AppointNewManagerSuccessTest(){
+        systemService.logOut("Andalus");
+        systemService.enterSystem();
+        String guestName = systemService.getOnline().getVal();
+        systemService.login(guestName, "Mira", "200");
+        Assert.assertFalse(systemService.addProductToStore("Mira", "Mega", "Bamba", 20, 5, "snacks").isSuccess());
+        systemService.logOut("Mira");
+        systemService.enterSystem();
+        guestName = systemService.getOnline().getVal();
+        systemService.login(guestName, "Andalus", "100");
+        systemService.appointStoreManager("Andalus", "Mega", "Mira");
+        Assert.assertTrue(systemService.addProductToStore("Mira", "Mega", "Bamba", 20, 5, "snacks").isSuccess());
+    }
+
+    @Test
+    public void AppointNewManagerFailTest() {
+        Assert.assertFalse(systemService.appointStoreManager("Andalus", "Mega", "Itay").isSuccess());
+    }
+
 }
--- EtradeSystem/src/Tests/Acceptance/User/Member/AppointNewOwnerTest.java
+++ EtradeSystem/src/Tests/Acceptance/User/Member/AppointNewOwnerTest.java
@@ -1,14 +1,50 @@
 package Tests.Acceptance.User.Member;
 
+import Service.SystemService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class AppointNewOwnerTest {
+
+    private SystemService systemService;
+
     @Before
     public void setUp() throws Exception {
+        systemService = new SystemService();
+        systemService.enterSystem();
+        String guestName = systemService.getOnline().getVal();
+        systemService.signUp(guestName, "Mira", "200");
+        systemService.signUp(guestName, "Andalus", "100");
+        systemService.signUp(guestName, "Andalus2", "102");
+        systemService.login(guestName, "Andalus", "100");
+        systemService.openStore("Andalus", "Mega", 123);
+        systemService.appointStoreManager("Andalus", "Mega", "Mira");
     }
 
     @After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void AppointNewOwnerSuccessTest(){
+        systemService.logOut("Andalus");
+        systemService.enterSystem();
+        String guestName = systemService.getOnline().getVal();
+        systemService.login(guestName, "Mira", "200");
+        Assert.assertFalse(systemService.appointStoreOwner("Mira", "Mega", "Andalus1").isSuccess());
+        systemService.logOut("Mira");
+        systemService.enterSystem();
+        guestName = systemService.getOnline().getVal();
+        systemService.login(guestName, "Andalus", "100");
+        systemService.appointStoreOwner("Andalus", "Mega", "Mira");
+        Assert.assertTrue(systemService.appointStoreOwner("Mira", "Mega", "Andalus1").isSuccess());
+    }
+
+    @Test
+    public void AppointNewOwnerFailTest() {
+        Assert.assertFalse(systemService.appointStoreOwner("Andalus", "Mega", "Itay").isSuccess());
+    }
+
 }
