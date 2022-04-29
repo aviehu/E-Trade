3f9011f5d1f3fba0078896df343b14862fa03854
sahar kalifa <kalifasahar>
2022-04-29 17:30:00 +0300

    update

--- EtradeSystem/src/Service/ServiceInterface.java
+++ EtradeSystem/src/Service/ServiceInterface.java
@@ -1,4 +1,84 @@
 package Service;
 
+import Domain.Stores.managersPermission;
+import Domain.purchaseOption;
+import Service.ResultPackge.ResultBool;
+import Service.ResultPackge.ResultMsg;
+
 public interface ServiceInterface {
+
+    public ResultBool addExternalPaymentService();
+
+    public ResultBool changeExternalPaymentService();
+
+    public ResultBool editExternalPaymentService();
+
+    public ResultBool addExternalSupplyService();
+
+    public ResultBool changeExternalSupplyService();
+
+    public ResultBool editExternalSupplyService();
+
+    public ResultBool exitSystem(String name);
+
+    public ResultBool exitSystemAsGuest(String name);
+
+    public ResultBool signUp(String userName, String password);
+
+    public ResultBool login(String userName, String password);
+
+    public ResultMsg getStoreInfo(String userName, String storeName);
+
+    public ResultMsg searchByKeyword(String userName, String keyword);
+
+    public ResultMsg searchByCategory(String userName, String category);
+
+    public ResultMsg searchByName(String userName, String productName);
+
+    public ResultBool addProductToShoppingCart(String userName, String productName, String storeName, int quantity);
+
+    public ResultMsg displayShoppingCart(String userName);
+
+    public ResultMsg addProductToShoppingCart(String userName);
+
+    public ResultMsg removeProductFromShoppingCart(String userName);
+
+    public ResultBool purchase(String userName);
+
+    public ResultBool logOut(String userName);
+
+    public ResultBool openStore(String founderName, String storeName, int card);
+
+    public ResultBool addProductToStore(String userName, String storeName, String productName, int amount, double price, String category);
+
+    public ResultBool removeProductFromStore(String userName, String storeName, String productName);
+
+    public ResultBool editProductName(String userName, String storeName, String oldProductName, String newProductName);
+
+    public ResultBool editProductPrice(String userName, String storeName, String productName, double newPrice);
+
+    public ResultBool editProductQuantity(String userName, String storeName, String productName, int newQuantity);
+
+    public ResultBool changePurchaseOption(String userName, String storeName, String productName, purchaseOption newOption);
+
+    public ResultBool appointStoreOwner(String userName, String storeName, String newOwner);
+
+    public ResultBool appointStoreManager(String userName, String storeName, String newManager);
+
+    public ResultBool changeStoreManagersPermission(String userName, String storeName, String managerName, managersPermission newPermission);
+
+    public ResultBool closeStore(String userName, String storeName);
+
+    public ResultMsg getStoresManagement(String userName, String storeName);
+
+    public ResultMsg getStoresPurchaseHistory(String userName, String storeName);
+
+    public ResultBool adminCloseStorePermanently(String adminName, String storeName);
+
+    public ResultBool adminTerminateUser(String adminName, String userToTerminate);
+
+    public ResultBool adminGetStoresPurchaseHistory(String adminName, String storeName);
+
+    public ResultBool exitSystem();
+
 }
--- EtradeSystem/src/Service/SystemService.java
+++ EtradeSystem/src/Service/SystemService.java
@@ -1,4 +1,193 @@
 package Service;
 
+import Domain.Stores.managersPermission;
+import Domain.purchaseOption;
+import Service.ResultPackge.ResultBool;
+import Service.ResultPackge.ResultMsg;
+
 public class SystemService implements ServiceInterface{
+    @Override
+    public ResultBool addExternalPaymentService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool changeExternalPaymentService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool editExternalPaymentService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool addExternalSupplyService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool changeExternalSupplyService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool editExternalSupplyService() {
+        return null;
+    }
+
+    @Override
+    public ResultBool exitSystem(String name) {
+        return null;
+    }
+
+    @Override
+    public ResultBool exitSystemAsGuest(String name) {
+        return null;
+    }
+
+    @Override
+    public ResultBool signUp(String userName, String password) {
+        return null;
+    }
+
+    @Override
+    public ResultBool login(String userName, String password) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg getStoreInfo(String userName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg searchByKeyword(String userName, String keyword) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg searchByCategory(String userName, String category) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg searchByName(String userName, String productName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool addProductToShoppingCart(String userName, String productName, String storeName, int quantity) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg displayShoppingCart(String userName) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg addProductToShoppingCart(String userName) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg removeProductFromShoppingCart(String userName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool purchase(String userName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool logOut(String userName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool openStore(String founderName, String storeName, int card) {
+        return null;
+    }
+
+    @Override
+    public ResultBool addProductToStore(String userName, String storeName, String productName, int amount, double price, String category) {
+        return null;
+    }
+
+    @Override
+    public ResultBool removeProductFromStore(String userName, String storeName, String productName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool editProductName(String userName, String storeName, String oldProductName, String newProductName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool editProductPrice(String userName, String storeName, String productName, double newPrice) {
+        return null;
+    }
+
+    @Override
+    public ResultBool editProductQuantity(String userName, String storeName, String productName, int newQuantity) {
+        return null;
+    }
+
+    @Override
+    public ResultBool changePurchaseOption(String userName, String storeName, String productName, purchaseOption newOption) {
+        return null;
+    }
+
+    @Override
+    public ResultBool appointStoreOwner(String userName, String storeName, String newOwner) {
+        return null;
+    }
+
+    @Override
+    public ResultBool appointStoreManager(String userName, String storeName, String newManager) {
+        return null;
+    }
+
+    @Override
+    public ResultBool changeStoreManagersPermission(String userName, String storeName, String managerName, managersPermission newPermission) {
+        return null;
+    }
+
+    @Override
+    public ResultBool closeStore(String userName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg getStoresManagement(String userName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultMsg getStoresPurchaseHistory(String userName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool adminCloseStorePermanently(String adminName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool adminTerminateUser(String adminName, String userToTerminate) {
+        return null;
+    }
+
+    @Override
+    public ResultBool adminGetStoresPurchaseHistory(String adminName, String storeName) {
+        return null;
+    }
+
+    @Override
+    public ResultBool exitSystem() {
+        return null;
+    }
 }
