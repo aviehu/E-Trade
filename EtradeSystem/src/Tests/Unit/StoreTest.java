90357f12d94f672ad75ef703978422132eb7e0c0
BatyaBialik <bialikb@post.bgu.ac.il>
2022-04-30 13:46:30 +0300

    start unit tests

---
+++ EtradeSystem/src/Tests/Unit/StoreTest.java
@@ -0,0 +1,102 @@
+package Tests.Unit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StoreTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void changeStoreManagersPermission() {
+    }
+
+    @Test
+    public void closeStore() {
+    }
+
+    @Test
+    public void adminCloseStore() {
+    }
+
+    @Test
+    public void purchase() {
+    }
+
+    @Test
+    public void getHistory() {
+    }
+
+    @Test
+    public void startAuction() {
+    }
+
+    @Test
+    public void addBid() {
+    }
+
+    @Test
+    public void startRaffle() {
+    }
+
+    @Test
+    public void addManager() {
+    }
+
+    @Test
+    public void addOwner() {
+    }
+
+    @Test
+    public void addProduct() {
+    }
+
+    @Test
+    public void removeOwner() {
+    }
+
+    @Test
+    public void removeManager() {
+    }
+
+    @Test
+    public void removeProduct() {
+    }
+
+    @Test
+    public void getAllProdsByCategory() {
+    }
+
+    @Test
+    public void searchByKeyword() {
+    }
+
+    @Test
+    public void searchByName() {
+    }
+
+    @Test
+    public void searchByCategory() {
+    }
+
+    @Test
+    public void rateStore() {
+    }
+
+    @Test
+    public void ask() {
+    }
+
+    @Test
+    public void changeProductQuantity() {
+    }
+}
