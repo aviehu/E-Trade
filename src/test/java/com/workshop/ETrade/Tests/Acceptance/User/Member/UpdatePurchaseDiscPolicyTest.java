dc75604c30ea71f44826a4991a08f8fa1d60e642
BatyaBialik <bialikb@post.bgu.ac.il>
2022-05-23 16:09:28 +0300

    tests- add dicount policy- not finished

--- src/test/java/com/workshop/ETrade/Tests/Acceptance/User/Member/UpdatePurchaseDiscPolicyTest.java
+++ src/test/java/com/workshop/ETrade/Tests/Acceptance/User/Member/UpdatePurchaseDiscPolicyTest.java
@@ -2,6 +2,7 @@ package com.workshop.ETrade.Tests.Acceptance.User.Member;
 
 import com.workshop.ETrade.Service.SystemService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ public class UpdatePurchaseDiscPolicyTest {
         guestName = systemService.enterSystem().getVal();
         systemService.login(guestName, "Andalus", "100");
         systemService.openStore("Andalus", "Mega", 123);
+        systemService.addProductToStore("Andalus", "Mega", "Bamba", 200, 5, "snacks");
     }
 
     @After
@@ -25,6 +27,10 @@ public class UpdatePurchaseDiscPolicyTest {
 
     @Test
     public void UpdatePurchaseDiscPolicySuccessTest(){
+        systemService.addProductToShoppingCart("Andalus", "Bamba", "Mega", 20);
+        //int price = getTotalPrice();
+        //להוסיף מדיניות הנחה
+        //Assert.assertNotEquals(price, getTotal());
        
 
     }
